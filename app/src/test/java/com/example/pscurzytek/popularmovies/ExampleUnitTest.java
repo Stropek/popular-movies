5a6d8dbe5676262ae99373627fcb2992e4bdeaf3
przemo <curzytekp@gmail.com>
2018-02-19 11:31:44 -0500

    change Kotlin files to Java

---
+++ app/src/test/java/com/example/pscurzytek/popularmovies/ExampleUnitTest.java
@@ -0,0 +1,19 @@
+package com.example.pscurzytek.popularmovies;
+
+import org.junit.Test;
+
+
+import static junit.framework.Assert.assertEquals;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+public class ExampleUnitTest {
+
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
