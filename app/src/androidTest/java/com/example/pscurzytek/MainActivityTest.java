3a1fe52d4ac21570440095349df1ab351a09c74f
przemo <curzytekp@gmail.com>
2018-02-23 21:50:21 -0500

    try to add instrumented test

--- app/src/androidTest/java/com/example/pscurzytek/MainActivityTest.java
+++ app/src/androidTest/java/com/example/pscurzytek/MainActivityTest.java
@@ -1,8 +1,21 @@
 package com.example.pscurzytek;
 
-/**
- * Created by p.s.curzytek on 2/23/2018.
- */
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static junit.framework.Assert.assertEquals;
+
+@RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
+
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+        assertEquals("com.example.pscurzytek.popularmovies", appContext.getPackageName());
+    }
 }
