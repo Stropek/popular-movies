ef5f8142cf91bc839cf16113192be9efd65bce3c
przemo <curzytekp@gmail.com>
2018-03-19 23:08:37 -0400

    add main activity view for tablets

--- app/src/main/java/com/example/pscurzytek/popularmovies/activities/MainActivity.java
+++ app/src/main/java/com/example/pscurzytek/popularmovies/activities/MainActivity.java
@@ -7,16 +7,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.GridView;
 
 import com.example.pscurzytek.popularmovies.Constants;
 import com.example.pscurzytek.popularmovies.R;
 import com.example.pscurzytek.popularmovies.enums.SortOrder;
+import com.example.pscurzytek.popularmovies.fragments.MovieDetailsFragment;
 import com.example.pscurzytek.popularmovies.fragments.MovieListFragment;
+import com.example.pscurzytek.popularmovies.models.Movie;
 
 public class MainActivity extends AppCompatActivity {
 
     private SortOrder sortOrder = SortOrder.MostPopular;
     private Handler handler;
+    private boolean bigScreen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         handler = new Handler();
+        bigScreen = findViewById(R.id.movie_details_fl) != null;
 
         loadMainFragments();
     }
@@ -55,26 +60,46 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void loadMainFragments() {
-        Runnable pendingRunnable = new Runnable() {
-            public void run() {  // update the main content by replacing fragments
-                Fragment fragment = getHomeFragment();
-                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-                fragmentTransaction.setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out);
-                fragmentTransaction.replace(R.id.movie_list_fl, fragment);
-                fragmentTransaction.commitAllowingStateLoss();
-            }
-        };
+        Fragment listFragment = getListFragment();
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out);
+        fragmentTransaction.replace(R.id.movie_list_fl, listFragment);
+        fragmentTransaction.commit();
 
-        handler.post(pendingRunnable);
+        if (bigScreen) {
+//            Runnable detailsFragment = new Runnable() {
+//                public void run() {  // update the main content by replacing fragments
+//                    Fragment fragment = getDetailsFragment();
+//                    FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+//                    fragmentTransaction.setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out);
+//                    fragmentTransaction.replace(R.id.movie_details_fl, fragment);
+//                    fragmentTransaction.commitAllowingStateLoss();
+//                }
+//            };
+//
+//            handler.post(detailsFragment);
+        }
 
         invalidateOptionsMenu();
     }
 
-    private Fragment getHomeFragment() {
+    private Fragment getListFragment() {
         Fragment fragment = new MovieListFragment();
 
         Bundle bundle = new Bundle();
         bundle.putSerializable(Constants.BundleKeys.SortOrder, sortOrder);
+        fragment.setArguments(bundle);
+
+        return fragment;
+    }
+
+    private Fragment getDetailsFragment() {
+        Fragment fragment = new MovieDetailsFragment();
+        final GridView thumbnails = findViewById(R.id.thumbnails_grid);
+        Movie movie = (Movie) thumbnails.getItemAtPosition(0);
+
+        Bundle bundle = new Bundle();
+        bundle.putSerializable(Constants.BundleKeys.MovieDetails, movie);
         fragment.setArguments(bundle);
 
         return fragment;
