fa15ca6e0f574c73e292971103d60eada3052bed
przemo <curzytekp@gmail.com>
2018-04-14 23:38:01 -0400

    add data contract, db helper and provider

---
+++ app/src/main/java/com/example/pscurzytek/popularmovies/data/MovieContract.java
@@ -0,0 +1,16 @@
+package com.example.pscurzytek.popularmovies.data;
+
+import android.provider.BaseColumns;
+
+public class MovieContract {
+
+    public static final class MovieEntry implements BaseColumns {
+        public static final String TABLE_NAME = "movies";
+
+        public static final String COLUMN_TITLE = "title";
+        public static final String COLUMN_POSTER = "poster";
+        public static final String COLUMN_SYNOPSIS = "synopsis";
+        public static final String COLUMN_RATING = "rating";
+        public static final String COLUMN_RELEASE_DATE = "release_date";
+    }
+}
---
+++ app/src/main/java/com/example/pscurzytek/popularmovies/data/MovieDbHelper.java
@@ -0,0 +1,37 @@
+package com.example.pscurzytek.popularmovies.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class MovieDbHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "movies.db";
+    private static final int DATABASE_VERSION = 1;
+
+    public MovieDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        final String createMoviesEntriesTable = "CREATE TABLE " +
+                MovieContract.MovieEntry.TABLE_NAME + "(" +
+                MovieContract.MovieEntry._ID + " INTEGER PRIMARY KEY," +
+                MovieContract.MovieEntry.COLUMN_TITLE + " TEXT NOT NULL," +
+                MovieContract.MovieEntry.COLUMN_SYNOPSIS + " TEXT," +
+                MovieContract.MovieEntry.COLUMN_POSTER + " TEXT," +
+                MovieContract.MovieEntry.COLUMN_RELEASE_DATE + " TEXT," +
+                MovieContract.MovieEntry.COLUMN_RATING + " INT" +
+                ");";
+
+        db.execSQL(createMoviesEntriesTable);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+//        if (oldVersion < 2) {
+//            db.execSQL(DATABASE_ALTER_TABLE_1)
+//        }
+    }
+}
